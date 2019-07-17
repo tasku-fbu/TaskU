ae6bf6bdb7e3cbb7a5e07c48d89e582da713914d
Raymond Gyabeng <rhaypapenfuzz@fb.com>
2019-07-16 22:55:01 -0700

    storing signup info

--- TaskU/View Controllers/SignUpViewController.m
+++ TaskU/View Controllers/SignUpViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "SignUpViewController.h"
+#import "Parse/Parse.h"
 
 @interface SignUpViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *universityNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *phoneNumberTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UITextField *confirmPasswordTextField;
+
 
 @end
 
@@ -21,7 +30,33 @@
 - (IBAction)cancelButtonAction:(id)sender {
      [self dismissViewControllerAnimated:true completion:nil];
 }
-
+- (IBAction)signUpButtonAction:(id)sender {
+    
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameTextField.text;
+    newUser.email = self.emailTextField.text;
+    newUser.password = self.passwordTextField.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            PFUser *user = [PFUser currentUser];
+            user[@"name"] = self.universityNameTextField.text;
+            user[@"university"] = self.universityNameTextField.text;
+            user[@"phone"] = self.phoneNumberTextField.text;
+            NSString *confirmPassword =  self.confirmPasswordTextField.text;
+            [user saveInBackground];
+            // dismiss signUpViewController
+            [self dismissViewControllerAnimated:true completion:nil];
+        }
+    }];
+}
 /*
 #pragma mark - Navigation
 
