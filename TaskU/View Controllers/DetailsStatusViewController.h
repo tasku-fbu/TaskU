a652dc413a22a3757d04af8edafb79265055985f
Yuewei Yuan <lucyyyw@fb.com>
2019-07-17 20:43:53 -0700

    create view controller files for details status and info

---
+++ TaskU/View Controllers/DetailsStatusViewController.h
@@ -0,0 +1,20 @@
+//
+//  DetailsStatusViewController.h
+//  TaskU
+//
+//  Created by lucyyyw on 7/17/19.
+//  Copyright © 2019 rhaypapenfuzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Task.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsStatusViewController : UIViewController
+
+@property (strong, nonatomic) Task *task;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TaskU/View Controllers/DetailsStatusViewController.m
@@ -0,0 +1,32 @@
+//
+//  DetailsStatusViewController.m
+//  TaskU
+//
+//  Created by lucyyyw on 7/17/19.
+//  Copyright © 2019 rhaypapenfuzz. All rights reserved.
+//
+
+#import "DetailsStatusViewController.h"
+
+@interface DetailsStatusViewController ()
+
+@end
+
+@implementation DetailsStatusViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
