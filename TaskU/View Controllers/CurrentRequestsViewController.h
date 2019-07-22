fcd43bc8da50afd57cd1e429820bdb25d9f4b410
Yuewei Yuan <lucyyyw@fb.com>
2019-07-22 12:18:01 -0700

    things showing up well in current request vc + network call and table views work well

--- TaskU/View Controllers/CurrentRequestsViewController.h
+++ TaskU/View Controllers/CurrentRequestsViewController.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface CurrentRequestsViewController : UIViewController
+@interface CurrentRequestsViewController : UIViewController <UITableViewDelegate,UITableViewDataSource>
 
 @end
 
--- TaskU/View Controllers/CurrentRequestsViewController.m
+++ TaskU/View Controllers/CurrentRequestsViewController.m
@@ -7,9 +7,16 @@
 //
 
 #import "CurrentRequestsViewController.h"
+#import "TaskCell.h"
+#import "DetailsStatusViewController.h"
+#import "DetailsViewController.h"
+#import "DetailsInfoViewController.h"
 
 @interface CurrentRequestsViewController ()
-
+@property (weak, nonatomic) IBOutlet UITableView *currentTable;
+@property (strong, nonatomic) NSMutableArray *currentTasks;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
 @end
 
 @implementation CurrentRequestsViewController
@@ -17,16 +24,127 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.currentTable.delegate = self;
+    self.currentTable.dataSource = self;
+    
+    [self getCurrentTasks];
+    
+    self.currentTable.rowHeight = UITableViewAutomaticDimension;
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(getCurrentTasks) forControlEvents:UIControlEventValueChanged];
+    [self.currentTable insertSubview:self.refreshControl atIndex:0];
+    
+    [self.activityIndicator startAnimating];
+}
+
+- (void) getCurrentTasks{
+    
+    PFUser *user = [PFUser currentUser];
+    PFQuery *query = [PFQuery queryWithClassName:@"Task"];
+    
+    NSArray *current = [NSArray arrayWithObjects: @"created",@"accepted", nil];
+    
+    [query orderByDescending:@"taskDate"];
+    [query includeKey:@"requester"];
+    [query includeKey:@"taskDate"];
+    [query includeKey:@"taskName"];
+    [query includeKey:@"taskDifficulty"];
+    [query includeKey:@"completionStatus"];
+    [query includeKey:@"missioner"];
+    [query includeKey:@"startAddress"];
+    [query includeKey:@"endAddress"];
+    [query includeKey:@"taskImage"];
+    [query includeKey:@"taskDescription"];
+    [query includeKey:@"category"];
+    [query includeKey:@"hours"];
+    [query includeKey:@"minutes"];
+    
+    [query whereKey:@"requester" equalTo:user];
+    [query whereKey:@"completionStatus" containedIn:current];
+    
+    //query.limit = 0;
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *tasks, NSError *error) {
+        if (tasks != nil) {
+            // do something with the array of object returned by the call
+            self.currentTasks = [tasks mutableCopy];
+            
+            [self.currentTable reloadData];
+            [self.refreshControl endRefreshing];
+            [self.activityIndicator stopAnimating];
+            
+        } else {
+            NSLog(@"%@", error.localizedDescription);
+        }
+        
+    }];
+}
+
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    TaskCell *cell = [tableView dequeueReusableCellWithIdentifier:@"currentRequest"];
+    //cell.delegate = self;
+    Task *task = self.currentTasks[indexPath.row];
+    cell.task = task;
+    cell.titleLabel.text = task[@"taskName"];
+    
+    PFUser *user = task[@"requester"];
+    cell.requesterLabel.text = [NSString stringWithFormat:@"@%@", user.username];
+    
+    NSNumber *payment = task[@"pay"];
+    int pay = [payment intValue];
+    cell.paymentLabel.text = [NSString stringWithFormat:@"$%i",pay];
+    
+    NSString *startString = @"";
+    if (task[@"startAddress"]) {
+        if (![task[@"startAddress"] isEqualToString:@""]) {
+            startString = [NSString stringWithFormat:@"FROM %@ ", task[@"startAddress"]];
+        }
+    }
+    cell.destinationLabel.text = [NSString stringWithFormat:@"%@TO %@",
+                                  startString,task[@"endAddress"]];
+    
+    
+    NSDate *date = task[@"taskDate"];
+    NSString *dateString = [self stringfromDateHelper:date];
+    cell.dateLabel.text = [NSString stringWithFormat:@"due %@", dateString];
+    
+    
+    NSNumber *hour = task[@"hours"];
+    NSNumber *minute = task[@"minutes"];
+    int hr = [hour intValue];
+    int min = [minute intValue];
+    if (hr == 0) {
+        cell.timeLabel.text = [NSString stringWithFormat:@"%imin", min];
+    } else if (min == 0){
+        cell.timeLabel.text = [NSString stringWithFormat:@"%ihr", hr];
+    } else {
+        cell.timeLabel.text = [NSString stringWithFormat:@"%ihr %imin", hr,min];
+    }
+    
+    return cell;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.currentTasks.count;
+}
+
+- (NSString *) stringfromDateHelper: (NSDate *) date {
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"HH:mm, MM.d, YYYY"];
+    NSString *dateString = [dateFormatter stringFromDate:date];
+    return dateString;
 }
 
 /*
-#pragma mark - Navigation
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
