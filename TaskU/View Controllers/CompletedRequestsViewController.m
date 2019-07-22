82ebb23c92d516cef7d7fe9aa87509065c9c7d17
Yuewei Yuan <lucyyyw@fb.com>
2019-07-22 12:41:43 -0700

    adding network call method for completed requests

--- TaskU/View Controllers/CompletedRequestsViewController.m
+++ TaskU/View Controllers/CompletedRequestsViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "CompletedRequestsViewController.h"
+#import "TaskCell.h"
 
 @interface CompletedRequestsViewController ()
-
+@property (weak, nonatomic) IBOutlet UITableView *completedTable;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (strong, nonatomic) NSMutableArray *completedTasks;
+@property (strong, nonatomic) UIRefreshControl *refreshControl;
 @end
 
 @implementation CompletedRequestsViewController
@@ -17,6 +21,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+
+- (void) getCompletedTasks{
+    PFUser *user = [PFUser currentUser];
+    PFQuery *query = [PFQuery queryWithClassName:@"Task"];
+    
+    NSArray *completed = [NSArray arrayWithObjects: @"completed",@"pay",@"paid", nil];
+    
+    [query orderByDescending:@"taskDate"];
+    [query includeKey:@"requester"];
+    [query includeKey:@"taskDate"];
+    [query includeKey:@"taskName"];
+    [query includeKey:@"taskDifficulty"];
+    [query includeKey:@"completionStatus"];
+    [query includeKey:@"missioner"];
+    [query includeKey:@"startAddress"];
+    [query includeKey:@"endAddress"];
+    [query includeKey:@"taskImage"];
+    [query includeKey:@"taskDescription"];
+    [query includeKey:@"category"];
+    [query includeKey:@"hours"];
+    [query includeKey:@"minutes"];
+    
+    [query whereKey:@"requester" equalTo:user];
+    [query whereKey:@"completionStatus" containedIn:completed];
+    
+    //query.limit = 0;
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *tasks, NSError *error) {
+        if (tasks != nil) {
+            // do something with the array of object returned by the call
+            self.completedTasks = [tasks mutableCopy];
+            
+            [self.completedTable reloadData];
+            [self.refreshControl endRefreshing];
+            [self.activityIndicator stopAnimating];
+            
+        } else {
+            NSLog(@"%@", error.localizedDescription);
+        }
+        
+    }];
+    
+    
+    
+    
 }
 
 /*
