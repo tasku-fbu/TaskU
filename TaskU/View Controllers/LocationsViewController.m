b0a544cf8351c484a0100ef6e6d2cc941c4f6a17
Raymond Gyabeng <rhaypapenfuzz@fb.com>
2019-07-22 10:30:37 -0700

    Added Scroll view in homepage, started the map from homepage when user presses the school name, and auto layout for login, also configures the collection cells to determine which timeline category is selected by user

---
+++ TaskU/View Controllers/LocationsViewController.m
@@ -0,0 +1,97 @@
+//
+//  LocationsViewController.m
+//  TaskU
+//
+//  Created by rhaypapenfuzz on 7/21/19.
+//  Copyright © 2019 rhaypapenfuzz. All rights reserved.
+//
+
+#import "LocationsViewController.h"
+#import "LocationCell.h"
+
+
+static NSString * const clientID = @"QA1L0Z0ZNA2QVEEDHFPQWK0I5F1DE3GPLSNW4BZEBGJXUCFL";
+static NSString * const clientSecret = @"W2AOE1TYC4MHK5SZYOUGX0J3LVRALMPB4CXT3ZH21ZCPUMCU";
+
+@interface LocationsViewController () <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (strong, nonatomic) NSArray *results;
+
+@end
+
+@implementation LocationsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.\
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    self.searchBar.delegate = self;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.results.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    LocationCell *cell = [tableView dequeueReusableCellWithIdentifier:@"LocationCell" forIndexPath:indexPath];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    // This is the selected venue
+    NSDictionary *venue = self.results[indexPath.row];
+    NSNumber *lat = [venue valueForKeyPath:@"location.lat"];
+    NSNumber *lng = [venue valueForKeyPath:@"location.lng"];
+    NSLog(@"%@, %@", lat, lng);
+    
+    //calling the locationsViewController delegate method with the latitude and longitude of the location the user selects.
+    [self.delegate locationsViewController:self didPickLocationWithLatitude:(lat) longitude:(lng)];
+    
+}
+//setting delegate in prepare for segue questioupd
+
+- (BOOL)searchBar:(UISearchBar *)searchBar shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    NSString *newText = [searchBar.text stringByReplacingCharactersInRange:range withString:text];
+    [self fetchLocationsWithQuery:newText nearCity:@"San Francisco"];
+    return true;
+}
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    [self fetchLocationsWithQuery:searchBar.text nearCity:@"San Francisco"];
+}
+
+- (void)fetchLocationsWithQuery:(NSString *)query nearCity:(NSString *)city {
+    NSString *baseURLString = @"https://api.foursquare.com/v2/venues/search?";
+    NSString *queryString = [NSString stringWithFormat:@"client_id=%@&client_secret=%@&v=20141020&near=%@,CA&query=%@", clientID, clientSecret, city, query];
+    queryString = [queryString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+    
+    NSURL *url = [NSURL URLWithString:[baseURLString stringByAppendingString:queryString]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (data) {
+            NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+            NSLog(@"response: %@", responseDictionary);
+            self.results = [responseDictionary valueForKeyPath:@"response.venues"];
+            [self.tableView reloadData];
+        }
+    }];
+    [task resume];
+}
+
+@end
---
+++ TaskU/View Controllers/chatApp/chatApp MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  chatApp MessagesExtension
+//
+//  Created by rhaypapenfuzz on 7/22/19.
+//  Copyright © 2019 rhaypapenfuzz. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
