7dc9bb4fb0e123aff51c06580440dc33096197f8
Yuewei Yuan <lucyyyw@fb.com>
2019-07-23 15:10:24 -0700

    creating relevant objective c files + link the storyboard reference + adjust the tab bar item title

---
+++ TaskU/View Controllers/ChatMessagesViewController.h
@@ -0,0 +1,17 @@
+//
+//  ChatMessagesViewController.h
+//  TaskU
+//
+//  Created by lucyyyw on 7/23/19.
+//  Copyright © 2019 rhaypapenfuzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChatMessagesViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TaskU/View Controllers/ChatMessagesViewController.m
@@ -0,0 +1,32 @@
+//
+//  ChatMessagesViewController.m
+//  TaskU
+//
+//  Created by lucyyyw on 7/23/19.
+//  Copyright © 2019 rhaypapenfuzz. All rights reserved.
+//
+
+#import "ChatMessagesViewController.h"
+
+@interface ChatMessagesViewController ()
+
+@end
+
+@implementation ChatMessagesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
