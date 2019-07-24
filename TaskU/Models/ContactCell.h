416167ca9094bebc4f24c4dbe476fc5775ad4fae
Yuewei Yuan <lucyyyw@fb.com>
2019-07-23 23:11:27 -0700

    connecting outlets for cells relevant to message

--- TaskU/Models/ContactCell.h
+++ TaskU/Models/ContactCell.h
@@ -11,6 +11,9 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ContactCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *contactProfileImageView;
+@property (weak, nonatomic) IBOutlet UILabel *contactUsernameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *latestTextLabel;
 
 @end
 
--- TaskU/Models/YourMessageCell.h
+++ TaskU/Models/YourMessageCell.h
@@ -11,6 +11,8 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface YourMessageCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *yourImageView;
+@property (weak, nonatomic) IBOutlet UILabel *YourTextLabel;
 
 @end
 
