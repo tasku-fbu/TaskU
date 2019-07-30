7f54ee5e2604127da322143f73f5683734bd33c6
Yuewei Yuan <lucyyyw@fb.com>
2019-07-30 11:00:49 -0700

    changing text font + starting with all elements inside the taskCell

--- TaskU/Models/TaskCell.h
+++ TaskU/Models/TaskCell.h
@@ -23,6 +23,8 @@ NS_ASSUME_NONNULL_BEGIN
 @property (weak, nonatomic) IBOutlet UILabel *dateLabel;
 @property (weak, nonatomic) IBOutlet UILabel *destinationLabel;
 @property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *requesterProfile;
+@property (weak, nonatomic) IBOutlet UILabel *startLabel;
 
 @property (strong, nonatomic) Task *task;
 
