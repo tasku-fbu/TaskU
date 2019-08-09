0a8f6490916d7d08cc8c121cc94e04c4f4cd1ca9
Yuewei Yuan <lucyyyw@fb.com>
2019-08-09 13:06:32 -0700

    editing the details navigation bar title and back button image

--- TaskU/View Controllers/DetailsViewController.m
+++ TaskU/View Controllers/DetailsViewController.m
@@ -7,72 +7,25 @@
 //
 
 #import "DetailsViewController.h"
-#import "PanTabAnimator.h"
-#import "VCTransitionsLibrary/CEHorizontalSwipeInteractionController.h"
 
 
-@interface DetailsViewController () <UITabBarControllerDelegate>
+@interface DetailsViewController ()
 
 
 @end
 
 @implementation DetailsViewController
-/*{
-    PanTabAnimator *_animationController;
-    CEHorizontalSwipeInteractionController *_swipeInteractionController;
-}
- */
-/*
-- (id)initWithCoder:(NSCoder *)aDecoder {
-    if (self = [super initWithCoder:aDecoder]) {
-        self.delegate = self;
-        _animationController = [PanTabAnimator new];
-        _swipeInteractionController = [CEHorizontalSwipeInteractionController new];
-        [self addObserver:self
-               forKeyPath:@"selectedViewController"
-                  options:NSKeyValueObservingOptionNew
-                  context:nil];
-    
-       
-    }
-    return self;
-}
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object
-                        change:(NSDictionary *)change
-                       context:(void *)context
-{
-    if ([keyPath isEqualToString:@"selectedViewController"] )
-    {
-        // wire the interaction controller to the view controller
-        [_swipeInteractionController wireToViewController:self.selectedViewController
-                                             forOperation:CEInteractionOperationTab];
-    }
-}
-
-
-
-- (id <UIViewControllerAnimatedTransitioning>)tabBarController:(UITabBarController *)tabBarController
-            animationControllerForTransitionFromViewController:(UIViewController *)fromVC
-                                              toViewController:(UIViewController *)toVC {
-    
-    NSUInteger fromVCIndex = [tabBarController.viewControllers indexOfObject:fromVC];
-    NSUInteger toVCIndex = [tabBarController.viewControllers indexOfObject:toVC];
-    
-    _animationController.reverse = fromVCIndex < toVCIndex;
-    return _animationController;
-}
-
--(id<UIViewControllerInteractiveTransitioning>)tabBarController:(UITabBarController *)tabBarController interactionControllerForAnimationController:(id<UIViewControllerAnimatedTransitioning>)animationController
-{
-    return _swipeInteractionController.interactionInProgress ? _swipeInteractionController : nil;
-}
- */
-- (void)viewDidLoad {
+- (void) viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    //[self.tabBar setSelectedItem:[[self.tabBar items] objectAtIndex:1]];
-    
+    //setting navigation bar
+    UINavigationBar *bar = [self.navigationController navigationBar];
+    [bar setBarTintColor:[UIColor colorWithRed:56/255.0 green:151.0/255 blue:240/255.0 alpha:1.0]];
+    bar.translucent = false;
+    bar.backgroundColor = [UIColor colorWithRed:56/255.0 green:151.0/255 blue:240/255.0 alpha:1.0];
+    [bar setValue:@(YES) forKeyPath:@"hidesShadow"];
+    [bar setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor whiteColor], NSFontAttributeName:[UIFont fontWithName:@"Quicksand-Bold" size:20]}];
 }
+
 - (IBAction)onTapBack:(id)sender {
     NSLog(@"on back");
     [self dismissViewControllerAnimated:YES completion:nil];
